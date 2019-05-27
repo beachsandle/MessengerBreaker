eae79308d2446f24cb041d2aa0a8b17ae1c33f65
irikong <gookirix@gmail.com>
2019-05-27 23:31:47 +0900

    Create Button

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ChangeGameScene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+        Application.OpenURL("https://github.com/beachsandle/MessengerBreaker");
+#else
+        Application.Quit();
+#endif
+    }
+}
